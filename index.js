94038d7045727a46b6bfd4eb085b744995cb5c14
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-11 22:17:37 +0300

    commit tree dee19c6fc36aa37032ac45be19a7ba426beb8edc

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceCovered) { // 42nd st
+    if (distanceCovered > 42) {
+        return distanceCovered - 42;
+    } else {
+        return 42 - distanceCovered;
+    }
+}
+
+function distanceFromHqInFeet(distanceCovered) {
+    if (distanceCovered > 42) {
+        let distanceInFeet = distanceCovered - 42
+        return distanceInFeet * 264;
+    } else {
+        let distanceInFeet = 42 - distanceCovered
+        return distanceInFeet * 264;
+    }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distanceCovered = (destination - start);
+    distanceCovered = Math.abs(distanceCovered);
+
+    if (distanceCovered == 5) {
+        return distanceCovered * 264;
+    } else if (distanceCovered == 10) {
+        return distanceCovered * 264;
+    } else {
+        return distanceCovered * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+
+    let distanceCovered = distanceTravelledInFeet(start, destination);
+
+    if (distanceCovered <= 400) {
+        
+        distanceCovered *=0;
+
+        return distanceCovered;
+
+    } else if (distanceCovered > 400 && distanceCovered <= 2000) {
+        
+        distanceCovered -=400;
+        
+        distanceCovered *=0.02;
+        
+        return distanceCovered;
+        
+    } else if (distanceCovered > 2000 && distanceCovered <= 2500) {
+
+        return 25;
+
+    } else {
+
+        return 'cannot travel that far'
+    }
+}
